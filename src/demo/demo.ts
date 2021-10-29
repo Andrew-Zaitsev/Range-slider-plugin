86b76bc00120546f8c78e1142d0a4891af6a8e5f
Andrew Zaitsev <andrew_lin@mail.ru>
2021-10-29 16:39:00 +0300

    Add demoSliderInit class for creating demo page elements and initialising demo-sliders. Initialased demo-sliders using demo-configs

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,7 +1,7 @@
 import './demo.scss';
 import type { userOptions } from '../facade/model/optionsTypes';
 
-const options: userOptions[] = [
+const demoSliderConfigs: userOptions[] = [
   {
     minValue: 10,
     maxValue: 50,
@@ -25,6 +25,44 @@ const options: userOptions[] = [
     step: 12,
   },
 ];
-$('.demo__title').rangeSlider(options[0]);
+
+class DemoSliderInit {
+  private demoSliderTitle: HTMLElement = document.querySelector('.demo__title');
+
+  private demoSliderSection: HTMLElement;
+
+  private demoSliderWrapper: HTMLElement;
+
+  private demoSliderControls: HTMLElement;
+
+  private demoSlider: HTMLElement;
+
+  constructor(demoSliderOptions: userOptions) {
+    this.init(demoSliderOptions);
+  }
+
+  private init(options: userOptions) {
+    this.demoSliderSection = document.createElement('section');
+    this.demoSliderSection.classList.add('demo__slider-section');
+
+    this.demoSliderWrapper = document.createElement('div');
+    this.demoSliderWrapper.classList.add('demo__slider-wrapper');
+
+    this.demoSliderControls = document.createElement('div');
+    this.demoSliderControls.classList.add('demo__slider-controls');
+
+    this.demoSlider = document.createElement('div');
+    this.demoSlider.classList.add('demo__slider', 'js-demo__slider');
+
+    this.demoSliderWrapper.append(this.demoSliderControls, this.demoSlider);
+    this.demoSliderSection.append(this.demoSliderWrapper);
+    this.demoSliderTitle.after(this.demoSliderSection);
+
+    // console.log(this.demoSlider);
+    $(this.demoSlider).rangeSlider(options);
+  }
+}
+
+demoSliderConfigs.forEach((config: userOptions) => new DemoSliderInit(config));
 
 // console.log(a.data());
